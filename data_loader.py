f7df3fed6db72c83c5ebc86f508102b03218e4b5
Jiehuihuang1107@163.com <Jiehuihuang1107@163.com>
2023-07-11 10:39:17 +0800

    CHANGE CONFIG

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,9 @@ from torch.utils.data import DataLoader, Dataset
 from transformers import BertTokenizer
 from create_dataset import MOSI, MOSEI, UR_FUNNY, PAD
 
-bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', mirror='https://huggingface.co/mirrors')
+
 
 class MSADataset(Dataset):
     def __init__(self, config):
